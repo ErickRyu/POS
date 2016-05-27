d98eb5aaafe7bc71479d60ebb2cae18e18480f2d
ErickRyu <rkggmdii@gmail.com>
2016-05-27 19:40:20 +0900

    Complete each UI component

--- src/UI/AddMenuPane.java
+++ src/UI/AddMenuPane.java
@@ -2,35 +2,27 @@ package UI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class AddMenuPane implements ActionListener {
 
-	private static Connection dbTest;
 
-	private String username;
-	private String password ;
-	private String loginName;
-	private String loginPw;
 	private JFrame frame = new JFrame();
 	private JPanel panel = new JPanel();
-	private JLabel idLabel = new JLabel("아이디");
-	private JLabel pwdLabel = new JLabel("비밀번호");
-	private JTextField idInput = new JTextField();
-	private JPasswordField pwdInput = new JPasswordField();
-	private JButton loginButton = new JButton("로그인");
+	
+	private JLabel nameLabel = new JLabel("메뉴명");
+	private JLabel priceLabel = new JLabel("가격");
+	
+	private JTextField nameInput = new JTextField();
+	private JTextField priceInput = new JTextField();
+	
+	private JButton addButton = new JButton("등록");
+	private JButton cancelButton = new JButton("취소");
 
 	public static void main(String[] args) {
 		new AddMenuPane();
@@ -40,67 +32,39 @@ public class AddMenuPane implements ActionListener {
 
 		panel.setLayout(null);
 
-		idLabel.setBounds(20, 10, 60, 30);
-		pwdLabel.setBounds(20, 50, 60, 30);
-		idInput.setBounds(100, 10, 80, 30);
-		pwdInput.setBounds(100, 50, 80, 30);
-		loginButton.setBounds(200, 25, 80, 35);
+		nameLabel.setBounds(20, 10, 100, 30);
+		priceLabel.setBounds(20, 50, 100, 30);
+		
+		nameInput.setBounds(110, 10, 120, 30);
+		priceInput.setBounds(110, 50, 120, 30);
+		
+		addButton.setBounds(10, 100, 100, 35);
+		cancelButton.setBounds(130, 100, 100, 35);
+		addButton.addActionListener(this);
+		cancelButton.addActionListener(this);
 
-		loginButton.addActionListener(this);
-
-		panel.add(idLabel);
-		panel.add(pwdLabel);
-		panel.add(idInput);
-		panel.add(pwdInput);
-		panel.add(loginButton);
+		
+		panel.add(nameLabel);
+		panel.add(priceLabel);
+		
+		panel.add(nameInput);
+		panel.add(priceInput);
+		
+		panel.add(addButton);
+		panel.add(cancelButton);
 
 		frame.add(panel);
 
-		frame.setTitle("Login");
-		frame.setSize(320, 130);
+		frame.setTitle("메뉴등록");
+		frame.setSize(280, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == loginButton) {
-			username = idInput.getText();
-			password = new String(pwdInput.getPassword());
-			connectDB();
+		
+		if(e.getSource() == cancelButton){
+			frame.setVisible(false);
 		}
 	}
-
-	private void connectDB() {
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			dbTest = DriverManager.getConnection("jdbc:oracle:thin:" + "@localhost:1521:XE", username, password);
-            JOptionPane.showMessageDialog(null, (String)"로그인 성공","메시지",2);
-            frame.dispose();
-
-		} catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, (String)"ID, PW를 확인해주세요 ","메시지",2);
-		} catch (Exception e) {
-			System.out.println("Exception:" + e);
-		}
-	}
-
-	public void executeSQL() {
-		try {
-			String sqlStr = "select id from userdatabase " + "where id='" + loginName + "' and pw='" + loginPw + "'";
-			PreparedStatement stmt = dbTest.prepareStatement(sqlStr);
-			ResultSet rs = stmt.executeQuery();
-
-			if (rs.next()) {
-				System.out.println("Login");
-			} else {
-				System.out.println("Fail");
-			}
-			rs.close();
-			stmt.close();
-		} catch (SQLException e) {
-			System.out.println("SQLException:" + e);
-			e.printStackTrace();
-		}
-	}
-
 }
--- src/UI/AddStaffPane.java
+++ src/UI/AddStaffPane.java
@@ -2,35 +2,27 @@ package UI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class AddStaffPane implements ActionListener {
 
-	private static Connection dbTest;
-
-	private String username;
-	private String password ;
-	private String loginName;
-	private String loginPw;
 	private JFrame frame = new JFrame();
 	private JPanel panel = new JPanel();
-	private JLabel idLabel = new JLabel("아이디");
-	private JLabel pwdLabel = new JLabel("비밀번호");
-	private JTextField idInput = new JTextField();
-	private JPasswordField pwdInput = new JPasswordField();
-	private JButton loginButton = new JButton("로그인");
+	
+	private JLabel nameLabel = new JLabel("직원명");
+	private JLabel positionLabel = new JLabel("직급");
+	
+	private JTextField nameInput = new JTextField();
+	private JComboBox<String> positionInput = new JComboBox<>();
+	
+	private JButton addButton = new JButton("등록");
+	private JButton cancelButton = new JButton("취소");
 
 	public static void main(String[] args) {
 		new AddStaffPane();
@@ -40,66 +32,39 @@ public class AddStaffPane implements ActionListener {
 
 		panel.setLayout(null);
 
-		idLabel.setBounds(20, 10, 60, 30);
-		pwdLabel.setBounds(20, 50, 60, 30);
-		idInput.setBounds(100, 10, 80, 30);
-		pwdInput.setBounds(100, 50, 80, 30);
-		loginButton.setBounds(200, 25, 80, 35);
-
-		loginButton.addActionListener(this);
-
-		panel.add(idLabel);
-		panel.add(pwdLabel);
-		panel.add(idInput);
-		panel.add(pwdInput);
-		panel.add(loginButton);
+		positionInput.addItem("Staff");
+		positionInput.addItem("Supervisor");
+		nameLabel.setBounds(20, 10, 100, 30);
+		positionLabel.setBounds(20, 50, 100, 30);
+		
+		nameInput.setBounds(110, 10, 120, 30);
+		positionInput.setBounds(110, 50, 120, 30);
+		
+		addButton.setBounds(10, 100, 100, 35);
+		cancelButton.setBounds(130, 100, 100, 35);
+		addButton.addActionListener(this);
+		cancelButton.addActionListener(this);
+		
+		panel.add(nameLabel);
+		panel.add(positionLabel);
+		
+		panel.add(nameInput);
+		panel.add(positionInput);
+		
+		panel.add(addButton);
+		panel.add(cancelButton);
 
 		frame.add(panel);
 
-		frame.setTitle("Login");
-		frame.setSize(320, 130);
+		frame.setTitle("직원등록");
+		frame.setSize(280, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == loginButton) {
-			username = idInput.getText();
-			password = new String(pwdInput.getPassword());
-			connectDB();
-		}
-	}
-
-	private void connectDB() {
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			dbTest = DriverManager.getConnection("jdbc:oracle:thin:" + "@localhost:1521:XE", username, password);
-            JOptionPane.showMessageDialog(null, (String)"로그인 성공","메시지",2);
-            frame.dispose();
-
-		} catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, (String)"ID, PW를 확인해주세요 ","메시지",2);
-		} catch (Exception e) {
-			System.out.println("Exception:" + e);
-		}
-	}
-
-	public void executeSQL() {
-		try {
-			String sqlStr = "select id from userdatabase " + "where id='" + loginName + "' and pw='" + loginPw + "'";
-			PreparedStatement stmt = dbTest.prepareStatement(sqlStr);
-			ResultSet rs = stmt.executeQuery();
-
-			if (rs.next()) {
-				System.out.println("Login");
-			} else {
-				System.out.println("Fail");
-			}
-			rs.close();
-			stmt.close();
-		} catch (SQLException e) {
-			System.out.println("SQLException:" + e);
-			e.printStackTrace();
+		if(e.getSource() == cancelButton){
+			frame.setVisible(false);
 		}
 	}
 
